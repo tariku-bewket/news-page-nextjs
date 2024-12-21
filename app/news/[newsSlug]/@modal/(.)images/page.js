72c654762022b8249ac02c5aab73255730332587
Tariku Bewket <tarikubew@gmail.com>
2024-12-21 14:58:41 +0300

    navigate programmatically using useRouter

--- app/news/[newsSlug]/@modal/(.)images/page.js
+++ app/news/[newsSlug]/@modal/(.)images/page.js
@@ -1,8 +1,12 @@
-import { notFound } from 'next/navigation';
+'use client';
+
+import { notFound, useRouter } from 'next/navigation';
 
 import { DUMMY_NEWS } from '@/data/dummy-news';
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
+
   const newsSlug = params.newsSlug;
   const news = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
 
@@ -11,7 +15,7 @@ export default function InterceptedImagePage({ params }) {
   }
   return (
     <>
-      <div className='modal-backdrop' />
+      <div className='modal-backdrop' onClick={router.back} />
       <dialog className='modal' open>
         <div id='fullscreen-image'>
           <img src={`/images/news/${news.image}`} alt={news.title} />
