aee0d754cd383bbcf45531e9cdf284e26d2f0152
Tariku Bewket <tarikubew@gmail.com>
2024-12-21 15:29:58 +0300

    build api with route handler

---
+++ app/api/route.js
@@ -0,0 +1,10 @@
+export function GET(request) {
+  // console.log(request);
+
+  return new Response('Hello from Next.js!');
+}
+
+export function POST(request) {
+  // console.log(request);
+  return new Response('Hello from Next.js!');
+}
